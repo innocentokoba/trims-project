1c00423183f11e69adbd5552001214d2be69c06f
innocent <innocentokoba@gmail.com>
2021-07-05 23:40:27 -0400

    Added buttons, landing page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(function() {
-    $("#appointment").on('click', function(){
+    $("#appointmentButton").on('click', function(){
         $('#appointmentModal').modal('show')
     });
     $("#memberLogin").on('click', function(){
         $('#memberLoginmodal').modal('show')
     });
+    $("#teamjoinButton").on('click', function(){
+        $('#teamjoinModal').modal('show')
+    });
+    
 });
+
